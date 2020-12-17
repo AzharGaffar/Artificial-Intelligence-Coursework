904f79cd0f4b7870129c30d6b9e0e1cff7fcf5ea
Abubakr Bhula <abubakr002@hotmail.com>
2020-12-17 21:52:03 +0000

    TASK DONE BY: ABUBAKR

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -10,11 +10,19 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import KFold
 from sklearn.model_selection import GridSearchCV
-#-----------------------------------------------------------------------------------------------------#
+
+
+# -----------------------------------------------------------------------------------------------------#
 # Baseline Model
 def baseline_model():
     # Splitting the data using train_test_split
+    # when doing document, put in random state
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+    # Standard Scaler is being used on X_train and then the transform is being used on X_test to reduce variance and make processing times quicker
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
 
     # Preparation of a Logistic Regression Model. Default Max Iter is 100
     logistic_regression_model = LogisticRegression()
@@ -41,7 +49,9 @@ def baseline_model():
     plt.xlabel("Predicted")
     plt.ylabel("True")
     plt.show()
-#-----------------------------------------------------------------------------------------------------#
+
+
+# -----------------------------------------------------------------------------------------------------#
 # Function for K-fold cross validation
 def k_fold_version(number_of_folds):
     # Preparation of a Logistic Regression Model. Default Max Iter is 100
@@ -51,10 +61,14 @@ def k_fold_version(number_of_folds):
     accuracy = 0
     loops = 0
 
+    # Preparation of standard scaler
+    scaler = StandardScaler()
+
     # 5 Folds
     kfolds = KFold(number_of_folds)
     for train_index, test_index in kfolds.split(X):
-        X_train, X_test = X[train_index], X[test_index]
+        # Standard Scaler is applied
+        X_train, X_test = scaler.fit_transform(X.iloc[train_index]), scaler.transform(X.iloc[test_index])
         y_train, y_test = y[train_index], y[test_index]
         logistic_regression_model.fit(X_train, y_train)
         predictions = logistic_regression_model.predict(X_test)
@@ -71,10 +85,18 @@ def k_fold_version(number_of_folds):
     # Calculating how much the model has missed the targets
     misclassification_rate = 1 - accuracy
     print("Your error rate with ", number_of_folds, "Fold Cross Validation is:", misclassification_rate * 100, "%")
-#-----------------------------------------------------------------------------------------------------#
+
+
+# -----------------------------------------------------------------------------------------------------#
 def best_test_model():
     # Splitting the data using train_test_split
+    # when doing document, put in random state
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+    # Standard Scaler is being used on X_train and then the transform is being used on X_test to reduce variance and make processing times quicker
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
 
     # Preparation of a Logistic Regression Model. Default Max Iter is 100
     logistic_regression_model = LogisticRegression()
@@ -96,19 +118,21 @@ def best_test_model():
     # Calculating how much the model has missed the targets
     misclassification_rate = 1 - accuracy
     print("Your error rate is", misclassification_rate * 100, "%")
-#-----------------------------------------------------------------------------------------------------#
+
+
+# -----------------------------------------------------------------------------------------------------#
 # Adding data
 df = pd.read_csv("high_diamond_ranked_10min.csv")
 
 # Dropping the correlated features
-df = df.drop(columns=['redGoldDiff','redExperienceDiff','blueDeaths','redGoldDiff','blueKills','blueGoldDiff','blueExperienceDiff','redFirstBlood','redKills','blueAvgLevel','blueTotalGold','blueTotalExperience','redDeaths','redEliteMonsters','redAssists','redDragons','blueEliteMonsters','blueAssists','blueTotalMinionsKilled','blueCSPerMin','redTotalMinionsKilled','redCSPerMin','redAvgLevel','redGoldPerMin'])
+df = df.drop(columns=['redGoldDiff', 'redExperienceDiff', 'blueDeaths', 'redGoldDiff', 'blueKills', 'blueGoldDiff',
+                      'blueExperienceDiff', 'redFirstBlood', 'redKills', 'blueAvgLevel', 'blueTotalGold',
+                      'blueTotalExperience', 'redDeaths', 'redEliteMonsters', 'redAssists', 'redDragons',
+                      'blueEliteMonsters', 'blueAssists', 'blueTotalMinionsKilled', 'blueCSPerMin',
+                      'redTotalMinionsKilled', 'redCSPerMin', 'redAvgLevel', 'redGoldPerMin'])
 
 # Storing the features in X
-X = df.drop(columns = 'blueWins')
-
-# Standard Scaler is being used to reduce variance and make processing times quicker
-scaler = StandardScaler().fit(X)
-X = scaler.transform(X)
+X = df.drop(columns='blueWins')
 
 # Setting the target variable
 y = df['blueWins']
@@ -119,10 +143,10 @@ baseline_model()
 k_fold_version(5)
 # Calling best test with 5 fold and finding the correct solver
 best_test_model()
-#-----------------------------------------------------------------------------------------------------#
+# -----------------------------------------------------------------------------------------------------#
 # Baseline accuracy without any iterations and WITHOUT STANDARD SCALER: 48.04%
 # Accuracy without any iterations and WITH STANDARD SCALER: 72.06%
 # Accuracy of 5 Fold Cross Validation resulted in: 72.84%
 # Adjusting solver hyper parameters using GridSearchCV with 5 Fold CV:
-    # Changing the max_iter parameter going up in increments of 10 up to 100 did not make a difference
-    # Changing the solver increased accuracy to: newton-cg resulted in the BEST accuracy sometimes of >73 (73.3% was max) but generally around mid 72%
+# Changing the max_iter parameter going up in increments of 10 up to 100 did not make a difference
+# Changing the solver increased accuracy to: newton-cg resulted in the BEST accuracy sometimes of >73 (73.3% was max) but generally around mid 72%
